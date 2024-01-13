41f6886b45c8de57bc3b2b9af4491ed9fc1192a1
庞晓豹 <e-Xiaobao.Pang@geely.com>
2024-01-13 16:55:24 +0800

    feat: 统计问题浏览

---
+++ lib/actions/interaction.action.ts
@@ -0,0 +1,33 @@
+'use server'
+
+import Question from '@/database/question.model'
+import { connectToDatabase } from '../mongoose'
+import { ViewQuestionParams } from './shared.types'
+import Interaction from '@/database/interaction.model'
+
+export async function viewQuestion(params: ViewQuestionParams) {
+  try {
+    connectToDatabase()
+    const { questionId, userId } = params
+    await Question.findByIdAndUpdate(questionId, {
+      $inc: { views: 1 }
+    })
+
+    if (userId) {
+      const existingInteraction = await Interaction.findOne({
+        user: userId,
+        action: 'view',
+        question: questionId
+      })
+      if (existingInteraction) return console.log('Already viewed')
+      await Interaction.create({
+        user: userId,
+        action: 'view',
+        question: questionId
+      })
+    }
+  } catch (error) {
+    console.log('🚀 ~ viewQuestion ~ error:', error)
+    throw error
+  }
+}
