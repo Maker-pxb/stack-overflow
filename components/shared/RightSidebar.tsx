d5e2a7df4b2917446574cdd82bb904960b7fed3e
庞晓豹 <e-Xiaobao.Pang@geely.com>
2024-01-21 17:03:51 +0800

    fix: 推荐问题

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -15,7 +15,7 @@ const RightSidebar = async () => {
           {hotQuestions.map((question) => {
             return (
               <Link
-                href={`/questions/${question._id}`}
+                href={`/question/${question._id}`}
                 key={question._id}
                 className="flex cursor-pointer items-center justify-between gap-7"
               >
